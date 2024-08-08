b8a82119b251339e8fde00d94f98b8d7bcfd5dfd
Krush-24 <138600135+Krush-24@users.noreply.github.com>
2024-08-08 12:18:04 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,33 @@
+// var a = parseInt(prompt("Enter the first number"));
+// var b = parseInt(prompt("Enter the second number"));
+// var op = prompt("Enter the operation to be done");
+// if(op=="+")
+// {
+//     console.log(a+b);
+// }
+// else if(op=="-")
+// {
+//     if(a>b)
+//         console.log(a-b);
+//     else
+//         console.log(b-a);
+// }
+// else if(op=="*")
+// {
+//     console.log(a*b);
+// }
+// else if(op=='/')
+// {
+//     if(b==0)
+//         console.log("can't divisible by 0");
+//     else
+//         console.log(a/b);
+// }
+// else
+// {
+//     console.log("Use only +, -, *, /");
+// }
+
+// // if the input values are 5 10 - = 5
+// // if the input values are 5 0 / = Can't divide by 0
+console.log("*"*5);
