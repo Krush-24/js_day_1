f4d859b5b5fdbcf944af44229deef808ee3bc638
Krush-24 <138600135+Krush-24@users.noreply.github.com>
2024-08-12 15:55:08 +0530

    Update design.js

--- design.js
+++ design.js
@@ -1,11 +1,11 @@
-// var a = parseInt(prompt("Enter the first number"));
-// var b = parseInt(prompt("Enter the second number"));
-// var op = prompt("Enter the operation to be done");
+// var a = parseInt(prompt("Enter n1"));
+// var b = parseInt(prompt("Enter n2"));
+// var op = prompt("Enter the Operation");
 // if(op=="+")
 // {
 //     console.log(a+b);
 // }
-// else if(op=="-")
+// else if(op=='-')
 // {
 //     if(a>b)
 //         console.log(a-b);
@@ -18,16 +18,46 @@
 // }
 // else if(op=='/')
 // {
-//     if(b==0)
-//         console.log("can't divisible by 0");
-//     else
-//         console.log(a/b);
+//     console.log(a/b);
 // }
 // else
 // {
-//     console.log("Use only +, -, *, /");
+//     alert("Use only +, -, *, /")
 // }
 
-// // if the input values are 5 10 - = 5
-// // if the input values are 5 0 / = Can't divide by 0
-console.log("*"*5);
+// console.log("10"*5);
+
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// var i = 1;
+// while(i<=5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// var i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+// var arr = [1,2,3,4,5];
+// console.log(arr);
+
+// let i =-1;
+// var arr = [1,2.34,'a','Hello',true];
+// arr[-1] = 'Starting';
+// arr[65] = 'A';
+// for(let i=-1;i<=10;i++);
+//     console.log(arr[i]);
+
+display(10,20);
+
+function display(a,b)
+{
+    console.log(a+b);
+}
